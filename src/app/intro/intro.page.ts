7335ca7ba4d02f134f1b9f3f691df2335d4d8c06
Samu1029 <angelsamudiolopez@gmail.com>
2023-07-09 13:23:53 -0500

    09/07/2023

--- src/app/intro/intro.page.ts
+++ src/app/intro/intro.page.ts
@@ -17,7 +17,7 @@ export class IntroPage implements OnInit {
     },
     {
       title: "Titulo Dos",
-      img: "assets/images/PlayStation-5.jpg",
+      img: "assets/img/PlayStation-5.jpg",
       icon: "barbell-outline",
       description: "Hola soy la explicacion del slide, Hola soy la explicacion del slide" 
     },
@@ -35,7 +35,7 @@ export class IntroPage implements OnInit {
     },
     {
       title: "Titulo Cinco",
-      img: "assets/images/bodega.jpg",
+      img: "assets/img/bodega.jpg",
       icon: "battery-full-outline",
       description: "Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas 'Letraset', las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum." 
     }
